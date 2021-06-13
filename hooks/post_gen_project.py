d6d65e5029594e027cfae23e1aaf0dbf6bb9d76c
CHOI HEE JAE <gmlwo530@gmail.com>
2021-06-13 23:20:53 +0900

    Add hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+
+print("Done!! Now, you can develop Django project!!")
