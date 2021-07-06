106ea3b1de7e1874c9b90b0537038884fc15f65f
CHOI HEE JAE <gmlwo530@gmail.com>
2021-07-06 20:25:43 +0900

    Add remove file logic in post gen project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,4 +9,8 @@ def remove(filepath):
         shutil.rmtree(filepath)
 
 
+if "{{ cookiecutter.open_source_license }}" == "Not open source":
+    remove(os.path.join(os.getcwd(), "{{cookiecutter.project_slug}}", "LICENSE"))
+
+
 print("Done!! Now, you can develop Django project!!")
